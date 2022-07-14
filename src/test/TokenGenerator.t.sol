c5af2cf4203c5a59448b9a403fd7236aff7683f1
Keyrxng <keyrxng@proton.me>
2022-07-14 19:48:25 +0100

    tests satisfy current needs will update as I add more features

--- src/test/TokenGenerator.t.sol
+++ src/test/TokenGenerator.t.sol
@@ -20,44 +20,139 @@ contract TokenGeneratorTest is Test {
     }
 
     function testDeployNewMockERC20() public {
-        bool res0 = generator.deployNewMockERC20("Test", "TST");
+        bool res0 = generator.deployNewMockERC20("TST", "TST");
         assertTrue(res0);
 
-        bool res1 = generator.deployNewMockERC20("Test", "TST");
+        MockERC20 token0 = generator.mintedERC20s(0);
+
+        address t0Owner = token0.owner();
+        console.log(t0Owner, address(this), msg.sender);
+        assertTrue(t0Owner != address(this));
+        // assertTrue(t0Owner != msg.sender);
+        cheats.warp(1);
+
+        /// 1st
+
+        bool res1 = generator.deployNewMockERC20("TST", "TST");
         assertTrue(res1);
 
-        bool res2 = generator.deployNewMockERC20("Test", "TST");
+        MockERC20 token1 = generator.mintedERC20s(1);
+
+        address t1Owner = token1.owner();
+
+        assertTrue(t1Owner != address(this));
+        // assertTrue(t1Owner != msg.sender);
+        cheats.warp(1);
+
+        /// 2nd
+
+        bool res2 = generator.deployNewMockERC20("TST", "TST");
         assertTrue(res2);
+
+        MockERC20 token2 = generator.mintedERC20s(2);
+
+        address t2Owner = token2.owner();
+
+        assertTrue(t2Owner != address(this));
+        // assertTrue(t2Owner != msg.sender);
+        cheats.warp(1);
+        /// 3rd
 
         uint256 tokens = generator.getMintedERC20s();
         uint256[] memory val = new uint256[](tokens);
         console.log(tokens);
     }
 
+    ////// ERC721
+
     function testDeployNewMockERC721() public {
-        bool res0 = generator.deployNewMockERC721("Test", "TST");
+        bool res0 = generator.deployNewMockERC721("TST", "TST");
         assertTrue(res0);
 
-        bool res1 = generator.deployNewMockERC721("Test", "TST");
+        MockERC721 token0 = generator.mintedERC721s(0);
+
+        address t0Owner = token0.owner();
+        console.log(t0Owner, address(this), msg.sender);
+
+        assertTrue(t0Owner != address(this));
+        cheats.warp(1);
+
+        /// 1st
+
+        bool res1 = generator.deployNewMockERC721("TST", "TST");
         assertTrue(res1);
 
-        bool res2 = generator.deployNewMockERC721("Test", "TST");
+        MockERC721 token1 = generator.mintedERC721s(1);
+
+        address t1Owner = token1.owner();
+
+        assertTrue(t1Owner != address(this));
+        // assertTrue(t1Owner != msg.sender);
+        cheats.warp(1);
+
+        /// 2nd
+
+        bool res2 = generator.deployNewMockERC721("TST", "TST");
         assertTrue(res2);
+
+        MockERC721 token2 = generator.mintedERC721s(2);
+
+        address t2Owner = token2.owner();
+
+        assertTrue(t2Owner != address(this));
+        // assertTrue(t2Owner != msg.sender);
+        console.log(t2Owner, address(this), msg.sender);
+        cheats.warp(1);
+
+        /// 3rd
 
         uint256 tokens = generator.getMintedERC721s();
         uint256[] memory val = new uint256[](tokens);
         console.log(tokens);
     }
 
+    ////// ERC1155
+
     function testDeployNewMockERC1155() public {
         bool res0 = generator.deployNewMockERC1155();
         assertTrue(res0);
 
+        MockERC1155 token0 = generator.mintedERC1155s(0);
+
+        address t0Owner = token0.owner();
+        console.log(t0Owner, address(this), msg.sender);
+        assertTrue(t0Owner != address(this));
+        assertTrue(t0Owner != msg.sender);
+        cheats.warp(1);
+
+        /// 1st
+
         bool res1 = generator.deployNewMockERC1155();
         assertTrue(res1);
 
+        MockERC1155 token1 = generator.mintedERC1155s(1);
+
+        address t1Owner = token1.owner();
+
+        assertTrue(t1Owner != address(this));
+        assertTrue(t1Owner != msg.sender);
+        cheats.warp(1);
+
+        /// 2nd
+
         bool res2 = generator.deployNewMockERC1155();
         assertTrue(res2);
+
+        MockERC1155 token2 = generator.mintedERC1155s(2);
+
+        address t2Owner = token2.owner();
+
+        assertTrue(t2Owner != address(this));
+        // assertTrue(t2Owner != msg.sender);
+        console.log(t2Owner, address(this), msg.sender);
+        cheats.warp(1);
+
+        /// 3rd
 
         uint256 tokens = generator.getMintedERC1155s();
         uint256[] memory val = new uint256[](tokens);
